23b594815c0f751280a4d48ae53f316f682b014d
nipplelion <piolotorrico@yahoo.com>
2023-04-21 17:42:01 -0700

    Add more user controls

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
-const screen = document.querySelector(".screen")
-const container = document.querySelector(".container");
-const gridSizeButton = document.querySelector(".grid-button");
+const screen = document.querySelector(".screen");
+const grid = document.querySelector(".grid");
+const gridSizeSlider = document.getElementById("grid-slider");
+const gridLabel = document.getElementById("grid-label");
+const clearButton = document.getElementById("clear-button");
+const colorPicker = document.getElementById("color");
+const eraserCheckbox = document.getElementById("eraser");
+const rainbowCheckbox = document.getElementById("rainbow");
+let color = colorPicker.value;
+let size = 50;
+let isEraser = false;
+let isRainbow = false;
 
-gridSizeButton.onclick = () => {
-    const size = parseInt(prompt("Please enter the number of rows and columns (max 100): "));
+gridSizeSlider.addEventListener("input", function() {
+    size = parseInt(gridSizeSlider.value);
+    gridLabel.textContent = gridSizeSlider.value + " x " + gridSizeSlider.value;
     if (size && size <= 100) {
-        createGrid(size, size)
+        createGrid(size, size);
     }
-}
+})
 
-function changeColor(square) {
+colorPicker.addEventListener("input", function() {
+    color = colorPicker.value;
+})
+
+clearButton.addEventListener("click", function() {
+    createGrid(size, size)
+})
+
+eraserCheckbox.addEventListener("change", function() {
+    isEraser = this.checked;
+})
+
+rainbowCheckbox.addEventListener("change", function() {
+    isRainbow = this.checked;
+})
+
+function randomColor(square) {
     let red = Math.floor(Math.random() * 256);
     let green = Math.floor(Math.random() * 256);
     let blue = Math.floor(Math.random() * 256);
     square.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
+function draw(square) {
+    if (isEraser) {
+        square.style.backgroundColor = "transparent";
+    } else if (isRainbow) {
+        randomColor(square);
+    } else {
+        square.style.backgroundColor = color;
+    }
+}
+
 function createGrid(row, col) {
-    container.innerHTML = '';
+    grid.innerHTML = '';
     for (let i = 0; i < row; i++) {
         const row = document.createElement("div");
         row.className = "row";
@@ -28,22 +64,22 @@ function createGrid(row, col) {
     
             square.addEventListener('mouseenter', (event) => {
                 if (event.buttons === 1) {
-                    changeColor(square);
+                    draw(square);
                 }
             })
 
                 
             square.addEventListener('mousedown', () => {
-                square.style.backgroundColor = "red"
+                draw(square);
             })
             
             row.appendChild(square);
         }
     
-        container.appendChild(row);
+        grid.appendChild(row);
     }
 }
 
-createGrid(10, 10);
+createGrid(size, size);
 
 
